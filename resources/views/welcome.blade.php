72fe05adba68b320efa5a198b722bf4e5066abab
Rohimhermawan <rohimhermawan01@gmail.com>
2022-02-04 18:07:27 +0700

    edit gambar dan satu menu kategori

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,26 +13,52 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     </head>
-    <body class="antialiased">
+    <body>
         <header>
-            <ul class="nav justify-content-center bg-">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Active</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
-                </li>
-            </ul>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Pejuang Ujian</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </nav>
         </header>
         <main>
-            <div class="mg-25">
-                <img src="/img/berkas/main.png" alt="main" class="img img-fluid">
+            <div class="container-fluid p-0" style="height: 500px; background-image: url(/img/berkas/main.png);">
+                <div class="w-100 h-100 d-flex align-items-center text-white" style="background-color: rgba(0, 0, 0, 0.747);">
+                    <div class="container text-center">
+                        <h1 class="">Selamat datang pejuang ujian!</h1>
+                        <h4 class="">Mari kita berjuang bersama-sama</h4>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container-fluid">
+                <div class="row justify-content-center mt-5">
+                    <div class="col-md-5 bg-dark text-white me-md-5"> 
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, quis perspiciatis in ex deleniti eius? Ab facere eveniet incidunt enim iusto fugiat, doloremque, ratione atque harum fuga, cum non quis sapiente natus quam. Aut, necessitatibus sunt nam unde quae odit cumque ut quaerat nobis delectus cum iusto deserunt error, voluptate amet rerum tempore alias. Assumenda odit eos quos molestias corporis rerum suscipit fugiat cupiditate placeat magnam ipsa aut dicta reiciendis libero, accusantium ratione ducimus itaque? Qui aut totam at consequatur repellendus pariatur doloremque quaerat, similique ad maxime sint dolores autem ipsum mollitia facere! Est facilis, rerum provident voluptas esse dolor.
+                    </div>
+                    <div class="col-md-5 bg-primary ms-md-5">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ratione architecto quis reprehenderit fugit laudantium. Itaque possimus culpa ut aspernatur cum cupiditate perspiciatis ducimus recusandae distinctio iure? Maxime voluptatibus ipsa perspiciatis deleniti ullam neque dignissimos optio atque aspernatur reprehenderit esse nemo voluptas repellendus magni consectetur quos eum ab, officia veritatis molestiae rerum! Sunt, cupiditate, perferendis ratione qui reiciendis fugiat praesentium, blanditiis saepe neque nostrum quisquam ducimus exercitationem amet officia ea perspiciatis maiores rerum magnam enim inventore. Soluta placeat quidem perferendis voluptate at cumque quos assumenda, officiis magni enim sit, ipsam delectus nihil dolor facere, sequi vitae dicta eum in! Repellat.
+                    </div>
+                </div>
             </div>
         </main>
 
