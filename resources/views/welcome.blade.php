a7ef40ee26d650bbbd3e950298d34f34b9af7f83
Rohimhermawan <rohimhermawan01@gmail.com>
2022-02-05 10:37:32 +0700

    edit features dan footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Pejuang Ujian</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -14,53 +14,85 @@
 
     </head>
     <body>
-        <header>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Pejuang Ujian</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
-                        </li>
-                    </ul>
-                    </div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Pejuang Ujian</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav mx-auto">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  <a class="nav-link" href="#">Features</a>
+                  <a class="nav-link" href="#">Pricing</a>
+                  <a class="nav-link disabled">Disabled</a>
                 </div>
-            </nav>
-        </header>
+              </div>
+            </div>
+          </nav>
         <main>
             <div class="container-fluid p-0" style="height: 500px; background-image: url(/img/berkas/main.png);">
                 <div class="w-100 h-100 d-flex align-items-center text-white" style="background-color: rgba(0, 0, 0, 0.747);">
                     <div class="container text-center">
-                        <h1 class="">Selamat datang pejuang ujian!</h1>
-                        <h4 class="">Mari kita berjuang bersama-sama</h4>
+                        <h1 class="">Welcome Warrior!</h1>
+                        <h4 class=""></h4>
                     </div>
                 </div>
             </div>
 
             <div class="container-fluid">
-                <div class="row justify-content-center mt-5">
-                    <div class="col-md-5 bg-dark text-white me-md-5"> 
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, quis perspiciatis in ex deleniti eius? Ab facere eveniet incidunt enim iusto fugiat, doloremque, ratione atque harum fuga, cum non quis sapiente natus quam. Aut, necessitatibus sunt nam unde quae odit cumque ut quaerat nobis delectus cum iusto deserunt error, voluptate amet rerum tempore alias. Assumenda odit eos quos molestias corporis rerum suscipit fugiat cupiditate placeat magnam ipsa aut dicta reiciendis libero, accusantium ratione ducimus itaque? Qui aut totam at consequatur repellendus pariatur doloremque quaerat, similique ad maxime sint dolores autem ipsum mollitia facere! Est facilis, rerum provident voluptas esse dolor.
+                <div class="row my-5">
+                    <div class="col-md-4 d-flex bg-dark text-white align-items-center">
+                        <div>
+                            <h2>Our Feature</h2>
+                            <p class="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet iusto praesentium distinctio commodi dicta tempore omnis tenetur, officiis quos, quod a magnam sequi error in veniam assumenda explicabo blanditiis unde impedit enim nesciunt minus ipsam. Repellat eaque laudantium error! Suscipit cupiditate cum, soluta sunt ab velit adipisci nobis modi maxime!</p>
+                        </div> 
                     </div>
-                    <div class="col-md-5 bg-primary ms-md-5">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ratione architecto quis reprehenderit fugit laudantium. Itaque possimus culpa ut aspernatur cum cupiditate perspiciatis ducimus recusandae distinctio iure? Maxime voluptatibus ipsa perspiciatis deleniti ullam neque dignissimos optio atque aspernatur reprehenderit esse nemo voluptas repellendus magni consectetur quos eum ab, officia veritatis molestiae rerum! Sunt, cupiditate, perferendis ratione qui reiciendis fugiat praesentium, blanditiis saepe neque nostrum quisquam ducimus exercitationem amet officia ea perspiciatis maiores rerum magnam enim inventore. Soluta placeat quidem perferendis voluptate at cumque quos assumenda, officiis magni enim sit, ipsam delectus nihil dolor facere, sequi vitae dicta eum in! Repellat.
+                    <div class="col-md-8">
+                        <div class="row justify-content-evenly">
+                            <div class="col-md-9">
+                                <img src="/img/berkas/blog.png" alt="" class="img-fluid">    
+                            </div>
+                            <div class="col-md-3 d-flex ms-auto justify-content-center align-items-center">
+                                <div class="text-center">
+                                    <h4>Blog</h4>
+                                    <a href="" class="btn btn-success">See more</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row flex-sm-row-reverse">
+                            <div class="col-md-9">
+                                <img src="/img/berkas/soal.png" alt="" class="img-fluid">    
+                            </div>
+                            <div class="col-md-3 d-flex ms-auto justify-content-center align-items-center">
+                                <div class="text-center">
+                                    <h4>Soal-Soal</h4>
+                                    <a href="" class="btn btn-success">See more</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </main>
+        <footer class="container-fluid">
+            <div class="row">
+                <div class="col-md-4 bg-dark">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, ratione aperiam. Nemo consequatur aut quas in doloribus at ut eos libero molestiae tenetur corporis ab a amet temporibus repellendus totam, placeat minus nobis tempore quod nesciunt? Quod mollitia laudantium repudiandae. Fuga iure eos magni modi at nobis nam voluptatibus voluptas ullam corrupti. Asperiores voluptatibus autem facilis sint ipsam officiis mollitia cupiditate possimus, eum aliquid accusamus rem. Odio pariatur est, facere reprehenderit, minus itaque iste quo iusto consequatur aliquid vitae illum, velit aperiam vel. Suscipit quia deserunt, vitae corporis et, fugit voluptatem a, tempora molestias error assumenda neque fuga? Dolor, veritatis?
+                </div>
+                <div class="col-md-4 bg-primary">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse in libero, laudantium quod saepe optio. Qui, quibusdam delectus. Perferendis ducimus assumenda doloribus quasi, incidunt, ea velit unde ipsam nam, quibusdam voluptatem reprehenderit? Dicta exercitationem hic ipsum facilis harum expedita magni rem ullam earum numquam quis illum eaque veniam, esse dolores iste dignissimos soluta eum accusamus! Quas soluta neque cumque, consequuntur saepe laudantium nihil! Voluptas, ducimus exercitationem! Dicta dolor molestias quisquam quis hic sapiente molestiae facilis praesentium nam quia natus blanditiis, nihil dolore eaque veritatis cumque mollitia, libero, itaque ipsa dolores in! Placeat ut accusamus dolorum odit ex, facere repellendus. Ut.
+                </div>
+                <div class="col-md-4 bg-secondary">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam, harum? Esse aliquam incidunt quo? Eius, ad atque architecto dolorum minima facilis itaque et veritatis maiores facere, eaque quo. Nulla culpa animi aspernatur quod vero illo incidunt voluptas atque eaque maxime! Fugit nostrum exercitationem corrupti porro, et commodi totam consequuntur mollitia omnis, natus sapiente, cupiditate tenetur ullam nobis ea ex velit nulla consectetur dolore. Mollitia voluptatum delectus est recusandae sint suscipit explicabo aspernatur veniam rem dicta minima fugiat et nihil, architecto ratione! Cupiditate eligendi quisquam quidem porro, architecto sint maiores, dignissimos dolores laboriosam blanditiis temporibus explicabo voluptates odio facilis iste impedit.
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 bg-warning">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora cum magnam fugiat reiciendis temporibus cupiditate ad amet architecto est nostrum optio, et, quo non hic recusandae! Deleniti architecto recusandae reiciendis est, ipsa, consectetur alias exercitationem nostrum similique dolore ex tempora consequuntur facilis corrupti. Reiciendis, nihil minima itaque eos id rerum est provident deleniti dolorem laboriosam doloremque voluptatum modi vitae exercitationem nobis, sunt rem, quod sint tempore. Molestiae sed necessitatibus quas omnis perspiciatis unde ad. Adipisci, corrupti magni impedit eaque consequuntur libero, beatae inventore exercitationem saepe aliquid quia nemo! Excepturi eligendi minus deleniti odit autem dolorem perferendis soluta consectetur amet aliquam.
+                </div>
+            </div>
+        </footer>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
